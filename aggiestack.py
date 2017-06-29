f2c31e12bdc296bb19dda80bf19e0585630ffbe8
Divyesh <tekale2@tamu.edu>
2017-06-29 06:44:49 +0000

    Finished partA with error checking and creating a logfile

--- aggiestack.py
+++ aggiestack.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 CONFIG_TYPES = ["HDWR","IMAGE","FLAVOR"]
 
@@ -14,12 +15,12 @@ class HdwrConfig:
         self.remDisks = int(numDisks)
         self.remCores = int(numCores)
     def showConfig(self):
-        infoStr = "Name: "+self.name+" IP: "+self.ip+" Mem: "+str(self.mem)+\
-        " NumDisks: "+str(self.numDisks)+" NumCores "+str(self.numCores)
+        infoStr = "Name:-> "+self.name+" IP:-> "+self.ip+" Mem:-> "+str(self.mem)+\
+        " NumDisks:-> "+str(self.numDisks)+" NumCores:-> "+str(self.numCores)
         print(infoStr)
     def showRemInfo(self):
-        infoStr = "Hdwr "+self.name+" has mem: "+str(self.remMem)+" disks: "+\
-        str(self.remDisks)+" cores: "+str(self.remCores)+" remaining"
+        infoStr = "Hdwr "+self.name+" has mem:-> "+str(self.remMem)+" disks:-> "+\
+        str(self.remDisks)+" cores:-> "+str(self.remCores)+" remaining"
         print(infoStr)
 
 # Stores static information about a single Image config
@@ -28,7 +29,7 @@ class ImageConfig:
         self.name = name
         self.path = path
     def showConfig(self):
-        infoStr = "Name: "+self.name+" Path: "+ self.path
+        infoStr = "Name:-> "+self.name+" Path:-> "+ self.path
         print(infoStr)
 
 # Stores static information about a single Flavor config
@@ -39,8 +40,8 @@ class FlavorConfig:
         self.numDisks = int(numDisks)
         self.numVCpus = int(numVCpus)
     def showConfig(self):
-        infoStr = "Name: "+self.name+" Mem: "+str(self.mem)+" NumDisks: "+\
-        str(self.numDisks)+" NumVCpus: "+str(self.numVCpus)
+        infoStr = "Name:-> "+self.name+" Mem:-> "+str(self.mem)+" NumDisks:-> "+\
+        str(self.numDisks)+" NumVCpus:-> "+str(self.numVCpus)
         print(infoStr)
 
 # stores information about a single instance
@@ -51,7 +52,7 @@ class Instance:
         self.imageName = imageName
         self.hdwrName = hdwrName
     def showInstanceInfo(self):
-        infoStr = "InstName: "+self.instName+" FlavorName: "+self.flavorName+\
+        infoStr = "InstName:-> "+self.instName+" FlavorName:-> "+self.flavorName+\
         " ImageName: "+self.imageName
         print(infoStr)
     def showHdwrInfo(self):
@@ -73,21 +74,25 @@ class CloudManager:
 
 # Function to load the configuration data from the file
     def loadConfigData(self, filename, configType):
-        with open(filename , 'r') as f:
-            configSize = int(f.readline())
-            for line in f:
-                info = [str(x) for x in line.split()]
-                if configType == CONFIG_TYPES[0]:
-                    CloudManager.HdwrConfigDict[info[0]] = HdwrConfig(info[0],\
-                                                info[1],info[2],info[3],info[4])
-                    CloudManager.CurrLoadList.append([info[0],0])
-                elif configType == CONFIG_TYPES[1]:
-                    CloudManager.ImageConfigDict[info[0]] = ImageConfig(info[0],\
-                                                          info[1])
-                else:
-                    CloudManager.FlavorConfigDict[info[0]] = FlavorConfig(info[0]\
-                                                       ,info[1],info[2],info[3])
-        return "Successfully loaded configuration from "+filename
+        if os.path.exists(filename):
+            with open(filename , 'r') as f:
+                configSize = int(f.readline())
+                for line in f:
+                    info = [str(x) for x in line.strip().split()]
+                    if configType == CONFIG_TYPES[0]:
+                        CloudManager.HdwrConfigDict[info[0]] = HdwrConfig(info[0],\
+                                                    info[1],info[2],info[3],info[4])
+                        CloudManager.CurrLoadList.append([info[0],0])
+                    elif configType == CONFIG_TYPES[1]:
+                        CloudManager.ImageConfigDict[info[0]] = ImageConfig(info[0],\
+                                                              info[1])
+                    else:
+                        CloudManager.FlavorConfigDict[info[0]] = FlavorConfig(info[0]\
+                                                           ,info[1],info[2],info[3])
+            return "SUCCESS"
+        else:
+            print "File "+filename+" does not exist"
+            return "FAILURE"
 
 # Function to show the configuration data
     def showConfigData(self, configType):
@@ -98,33 +103,52 @@ class CloudManager:
             configDict = CloudManager.ImageConfigDict
         else:
             configDict = CloudManager.FlavorConfigDict
+        if len(configDict) == 0:
+            print "Configuration was not loaded"
+            return "FAILURE"
         for name,config in configDict.iteritems():
             config.showConfig()
+        return "SUCCESS"
 
 # Function to show remaining Hdwr resources
     def showRemHdwr(self):
+        if len(CloudManager.HdwrConfigDict) == 0:
+            print "Hdwr Configuration was not loaded"
+            return "FAILURE"
         for name,config in CloudManager.HdwrConfigDict.iteritems():
             config.showRemInfo()
+        return "SUCCESS"
         
 # Function to list all info about the instances running
     def showInstances(self):
+        if len(CloudManager.InstanceDict) == 0:
+            print "No instances running"
+            return "FAILURE"
         for name,config in CloudManager.InstanceDict.iteritems():
             config.showInstanceInfo()
+        return "SUCCESS"
 
 # Function to show on which Hdwr each instance is running on
     def showHdwrInst(self):
+        if len(CloudManager.InstanceDict) == 0:
+            print "No instances running"
+            return "FAILURE"
         for name,config in CloudManager.InstanceDict.iteritems():
             config.showHdwrInfo()
+        return "SUCCESS"
         
 # Function to create a new instace
 # Using least connection method for load balancing
     def createInst(self,imageName, flavorName, instName):
         if instName in CloudManager.InstanceDict:
-            return instName+" already exists"
+            print(instName+" already exists")
+            return "FAILURE"
         elif flavorName not in CloudManager.FlavorConfigDict:
-            return "Invalid flavorName "+flavorName
+            print("Invalid flavorName "+flavorName)
+            return "FAILURE"
         elif imageName not in CloudManager.ImageConfigDict:
-            return "Invalid imageName "+imageName
+            print("Invalid imageName "+imageName)
+            return "FAILURE"
         # Find appropiate server for the instance
         CloudManager.CurrLoadList.sort(key=lambda tup:tup[1])
         for tup in CloudManager.CurrLoadList:
@@ -146,13 +170,15 @@ class CloudManager:
                        CloudManager.HdwrConfigDict[tup[0]].remDisks -=\
                        CloudManager.FlavorConfigDict[flavorName].numDisks
                        
-                       return "Successfully created instance "+instName
-        return "Resoucres exhausted, Instance "+instName+" cannot be created"
+                       return "SUCCESS"
+        print("Resoucres exhausted, Instance "+instName+" cannot be created")
+        return "FAILURE"
                        
 # Function to delete an existing instance
     def deleteInst(self,instName):
         if instName not in CloudManager.InstanceDict:
-            return instName+" does not exist"
+            print(instName+" does not exist")
+            return "FAILURE"
         hdwrName = CloudManager.InstanceDict[instName].hdwrName
         flavorName = CloudManager.InstanceDict[instName].flavorName
         
@@ -171,64 +197,82 @@ class CloudManager:
                 break
         # delete the instance entry
         del CloudManager.InstanceDict[instName]
-        return "Successfully deleted Instance "+instName
+        return "SUCCESS"
 
 
 # reads the input file and executes the commands
 def execCommands():
     cloudManager = CloudManager()
     inputFile = sys.argv[1]
-    msg =""
-    with open(inputFile,'r') as f:
-        for line in f:
-            params = [x for x in line.split()]
-            if len(params)<3:
-                continue
-# Parse the commands and execute them accordingly
-            if params[1].lower() == "config":
-                if params[2].lower() == "--hardware":
-                    msg = cloudManager.loadConfigData(params[3],CONFIG_TYPES[0])
-                elif params[2].lower() == "--images":
-                    msg = cloudManager.loadConfigData(params[3],CONFIG_TYPES[1])
-                elif params[2].lower() == "--flavors":
-                    msg = cloudManager.loadConfigData(params[3],CONFIG_TYPES[2])
-                print(msg)
-                
-            elif params[1].lower() == "show":
-                if params[2].lower() == "hardware":
-                    cloudManager.showConfigData(CONFIG_TYPES[0])
-                elif params[2].lower() == "images":
-                    cloudManager.showConfigData(CONFIG_TYPES[1])
-                elif params[2].lower() == "flavors":
-                    cloudManager.showConfigData(CONFIG_TYPES[2])
-
-            elif params[1].lower() == "server":
-                if params[2].lower() == "list":
-                    cloudManager.showInstances()
-                elif params[2].lower() == "delete":
-                    msg = cloudManager.deleteInst(params[3])
-                    print(msg)
-                elif params[2].lower() == "create":
-                    instName =""
-                    flavorName =""
-                    imageName =""
-                    for i in range(3,len(params)):
-                        if params[i].lower() == "--image":
-                            imageName = params[i+1]
-                            i+=1
-                        elif params[i].lower() == "--flavor":
-                            flavorName = params[i+1]
-                            i+=1
-                        else:
-                            instName = params[i]
-                    msg = cloudManager.createInst(imageName,flavorName,instName)
-                    print(msg)
-                    
-            elif params[1].lower() == "admin":
-                if params[2].lower() == "show" and params[3].lower() == "hardware":
-                    cloudManager.showRemHdwr()
-                elif params[2].lower() == "show" and params[3].lower() == "instances":
-                    cloudManager.showHdwrInst()
+    outfile = open("output.log","w")
+    retVal =""
+    if os.path.exists(inputFile):
+        with open(inputFile,'r') as f:
+            for line in f:
+                params = [x for x in line.strip().split()]
+                if len(params)<3:
+                    continue
+    # Parse the commands and execute them accordingly
+                if params[1].lower() == "config":
+                    if params[2].lower() == "--hardware":
+                        retVal = cloudManager.loadConfigData(params[3],CONFIG_TYPES[0])
+                    elif params[2].lower() == "--images":
+                        retVal = cloudManager.loadConfigData(params[3],CONFIG_TYPES[1])
+                    elif params[2].lower() == "--flavors":
+                        retVal = cloudManager.loadConfigData(params[3],CONFIG_TYPES[2])
+                    else:
+                        print("Invalid command")
+                        continue
+                    outfile.write(line.strip()+ " <"+retVal+">\n")
+        
+                elif params[1].lower() == "show":
+                    if params[2].lower() == "hardware":
+                        retVal = cloudManager.showConfigData(CONFIG_TYPES[0])
+                    elif params[2].lower() == "images":
+                        retVal = cloudManager.showConfigData(CONFIG_TYPES[1])
+                    elif params[2].lower() == "flavors":
+                        retVal = cloudManager.showConfigData(CONFIG_TYPES[2])
+                    else:
+                        print("Invalid command")
+                        continue
+                    outfile.write(line.strip()+ " <"+retVal+">\n")
+    
+                elif params[1].lower() == "server":
+                    if params[2].lower() == "list":
+                       retVal = cloudManager.showInstances()
+                    elif params[2].lower() == "delete":
+                        retVal = cloudManager.deleteInst(params[3])
+                    elif params[2].lower() == "create":
+                        instName =""
+                        flavorName =""
+                        imageName =""
+                        for i in range(3,len(params)):
+                            if params[i].lower() == "--image":
+                                imageName = params[i+1]
+                                i+=1
+                            elif params[i].lower() == "--flavor":
+                                flavorName = params[i+1]
+                                i+=1
+                            else:
+                                instName = params[i]
+                        retVal = cloudManager.createInst(imageName,flavorName,instName)
+                    else:
+                        print("Invalid command")
+                        continue
+                    outfile.write(line.strip()+ " <"+retVal+">\n")
+                        
+                elif params[1].lower() == "admin":
+                    if params[2].lower() == "show" and params[3].lower() == "hardware":
+                        retVal = cloudManager.showRemHdwr()
+                    elif params[2].lower() == "show" and params[3].lower() == "instances":
+                        retVal = cloudManager.showHdwrInst()
+                    else:
+                        print("Invalid command")
+                        continue
+                    outfile.write(line.strip()+ " <"+retVal+">\n")
+    else:
+        print("File "+inputFile+" does not exist")
+    outfile.close()
 
 if __name__ == "__main__":
     execCommands()
